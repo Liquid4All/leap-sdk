49708ae1520dc4ed2b457599aaa003308e6341cf
David Berrios <iamstuffed@gmail.com>
2026-03-11 02:49:30 -0700

    chore: update v0.10.0-SNAPSHOT checksums for CI-built xcframeworks with correct iOS simulator dylibs

--- Package.swift
+++ Package.swift
@@ -39,22 +39,22 @@ let package = Package(
         .binaryTarget(
             name: "LeapSDK",
             url: "https://github.com/Liquid4All/leap-sdk/releases/download/v0.10.0-SNAPSHOT/LeapSDK.xcframework.zip",
-            checksum: "16a8ff310987c8948d73b614cd6a0324e7b803b3670c9f1bfe850703c7c44061"
+            checksum: "288ef56d0e0833b4d533748dc90093929976088876cc7cd4d24e30b840097050"
         ),
         .binaryTarget(
             name: "LeapModelDownloader",
             url: "https://github.com/Liquid4All/leap-sdk/releases/download/v0.10.0-SNAPSHOT/LeapModelDownloader.xcframework.zip",
-            checksum: "b0d01213b0f0751fd1622e3f78eaabf6858e2bbfba378c2a7a8d2022cdbe361b"
+            checksum: "4db4f040208277c403284780687f1ba23fda4926c30966ec8f6bc07c9559d549"
         ),
         .binaryTarget(
             name: "LeapOpenAIClient",
             url: "https://github.com/Liquid4All/leap-sdk/releases/download/v0.10.0-SNAPSHOT/LeapOpenAIClient.xcframework.zip",
-            checksum: "14270cbfdf8f317d24c9dcefaae20e95a8fa5812e3c457d6552c447bb50b5d8b"
+            checksum: "e42604c5bf94d1a6f1baaaeec2e4f7ac64c13c7c433f04d2938baccb4db496a7"
         ),
         .binaryTarget(
             name: "LeapUi",
             url: "https://github.com/Liquid4All/leap-sdk/releases/download/v0.10.0-SNAPSHOT/LeapUi.xcframework.zip",
-            checksum: "4a64e5a3c3c568fb5c3d7f0d384765940592bf0e37e8943eb6c45348a84387bb"
+            checksum: "f08f8787463c601e1c2d100dc5fb0b461c8e0fed731139fec71085f8db2985d8"
         ),
         .macro(
             name: "LeapSDKConstrainedGenerationPlugin",
