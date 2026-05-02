38921a03c615d5e60e0607ed3847beb8c0564b43
David Berrios <iamstuffed@gmail.com>
2026-05-01 22:38:15 -0700

    chore: update Package.swift with SPM checksums for v0.10.3-SNAPSHOT

--- Package.swift
+++ Package.swift
@@ -39,22 +39,22 @@ let package = Package(
         .binaryTarget(
             name: "LeapSDK",
             url: "https://github.com/Liquid4All/leap-sdk/releases/download/v0.10.3-SNAPSHOT/LeapSDK.xcframework.zip",
-            checksum: "2b4a37fc626285aa2616599f295e354b58909f4805d3f9f68cc78fdbcfba468f"
+            checksum: "987d4f1b1941b9b523d6e18b17b4d7898b4b5c641fe08a65bc049c69e8348122"
         ),
         .binaryTarget(
             name: "LeapModelDownloader",
             url: "https://github.com/Liquid4All/leap-sdk/releases/download/v0.10.3-SNAPSHOT/LeapModelDownloader.xcframework.zip",
-            checksum: "9ccd69192784e973c6e237794eb58aa89beb0a638de24c8aefc942ee13a6d7c5"
+            checksum: "76c8a645ad4413abd8dfac30ae466145b97dd7e1dc1e189b05fe70ebe2356da4"
         ),
         .binaryTarget(
             name: "LeapOpenAIClient",
             url: "https://github.com/Liquid4All/leap-sdk/releases/download/v0.10.3-SNAPSHOT/LeapOpenAIClient.xcframework.zip",
-            checksum: "cc2471507c9fbac0de05681c6ba5a615eed3053c3d0bc3d0e03c31a41b786e1c"
+            checksum: "79bc5443a1cce6fcd4c49c91eeb85727034aaca10d3ef69582c061989c3d9b70"
         ),
         .binaryTarget(
             name: "LeapUi",
             url: "https://github.com/Liquid4All/leap-sdk/releases/download/v0.10.3-SNAPSHOT/LeapUi.xcframework.zip",
-            checksum: "f0af7659dcf92fa4b0284b055da69e03e5e17c509fb60e0b2ab3e2af9be4163c"
+            checksum: "3403c989b8464f8b84b1a88b169667b13391f7408ce3fb2f0c9c4139db7ecdc1"
         ),
         .macro(
             name: "LeapSDKConstrainedGenerationPlugin",
