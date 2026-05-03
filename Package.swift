c9644d86409544d240b38479cbb092bb964a3ca6
David Berrios <iamstuffed@gmail.com>
2026-05-03 07:41:37 -0700

    chore: rebuild v0.10.4-SNAPSHOT with f07a6d8 xcframeworks
    Force-replace v0.10.4-SNAPSHOT with leap-android-sdk run 25280958455
    artifacts (HEAD f07a6d8). The prior snapshot was built from a pre-f07a6d8
    commit and is missing the contextSize routing fix in the appleMain compat
    layer (LiquidInferenceEngineOptions / LiquidInferenceEngineManifestOptions).
    
    New checksums:
    - LeapSDK              db86840…
    - LeapModelDownloader  08b9e183…
    - LeapOpenAIClient     dbaa0640…
    - LeapUi               bb29644d…

--- Package.swift
+++ Package.swift
@@ -39,22 +39,22 @@ let package = Package(
         .binaryTarget(
             name: "LeapSDK",
             url: "https://github.com/Liquid4All/leap-sdk/releases/download/v0.10.4-SNAPSHOT/LeapSDK.xcframework.zip",
-            checksum: "4e4a8402d7714b2e915c0726efd42a3d662d3919edb3ac82d3ca323f70b9802e"
+            checksum: "db8684074bba348cd26f2750ac5d24b78f1de610d7eae692de42c53fd46a2b49"
         ),
         .binaryTarget(
             name: "LeapModelDownloader",
             url: "https://github.com/Liquid4All/leap-sdk/releases/download/v0.10.4-SNAPSHOT/LeapModelDownloader.xcframework.zip",
-            checksum: "03b313b1f9e17e22ce352e6038334fd1e71da166e610712f5838e5dd5812d5a4"
+            checksum: "08b9e183b2ed1327de96dbf1c5b09f68c0fa7f2b444ffa81fd7368ebf8118c0d"
         ),
         .binaryTarget(
             name: "LeapOpenAIClient",
             url: "https://github.com/Liquid4All/leap-sdk/releases/download/v0.10.4-SNAPSHOT/LeapOpenAIClient.xcframework.zip",
-            checksum: "4b8b641f5ce97818cbfa23b53bcfdd9361a44e3ae44146effcdabfc5ad6820a5"
+            checksum: "dbaa0640ab3d671044f8296bc0ee8f5299f9046c2f406ee72ba9726513f5a3c7"
         ),
         .binaryTarget(
             name: "LeapUi",
             url: "https://github.com/Liquid4All/leap-sdk/releases/download/v0.10.4-SNAPSHOT/LeapUi.xcframework.zip",
-            checksum: "596b3a388ad3be80f39dafae65e39d9657a343bece1349d64311f02befe46ca2"
+            checksum: "bb29644db65c4a1825260ad171227e9eb37ac9c0a0cdc528036db7ef2c187c96"
         ),
         .macro(
             name: "LeapSDKConstrainedGenerationPlugin",
